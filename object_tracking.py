b845e87f04c7615015ef40bdbe8d16215587c3ab
Danny <a7527744@gmail.com>
2023-07-12 11:32:35 +0800

    Fly to designated place

--- object_tracking.py
+++ object_tracking.py
@@ -9,11 +9,13 @@ import cv2
 import drone_kit
 from deep_sort.fuzzy_util import controller
 
-
 # 設定要捕獲的螢幕範圍（左上角座標和右下角座標）
-screen_coordinates = (1225,0 , 2550, 1080)  # 自行調整螢幕解析度
+screen_coordinates = (0, 0, 1920, 1080)  # 自行調整螢幕解析度
 
 drone_kit.arm_and_takeoff(100)
+drone_kit.goto(-35.360354, 149.160218, 100)
+drone_kit.condition_yaw(240, True, True)
+drone_kit.clear()
 
 model = ObjectRecognition()
 encoder = init_encoder()
@@ -28,9 +30,10 @@ while (True):
 
     if len(boxes) > 0:
         encoding = generate_detections(encoder, boxes, frame)
-        target_box_central=run_deep_sort(frame, encoding, config)
-        fuzzy_result = FuzzyController.run(target_box_central, frame.shape[0:2][::-1])
-
+        target_box_central = run_deep_sort(frame, encoding, config)
+        print(target_box_central)
+        if (target_box_central != None):
+            fuzzy_result = FuzzyController.run(target_box_central, frame.shape[0:2][::-1])
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
